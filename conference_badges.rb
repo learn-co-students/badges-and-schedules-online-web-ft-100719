6b739c91edc2ab0f30a64dee3aaa5f3ed3282186
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-07 20:40:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}.".to_s
+end
+  
+people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]  
+
+  def batch_badge_creator(people)
+      people.collect do |message|
+      badge_maker(message)
+   end
+ end
+  
+  def assign_rooms(people)
+    people.each_with_index.map do |name, index|
+     "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+ end
+  
+def printer(people)
+ batch_badge_creator(people).each do |badge|
+  puts badge
+ end
+ assign_rooms(people).each do |room|
+  puts room
+ end
+end
