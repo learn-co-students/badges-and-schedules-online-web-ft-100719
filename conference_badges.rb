5076d750b0994b9ca5e36507f2baa66f06b1d9df
Michael Calhoun <iengineersoftware@gmail.com>
2019-10-15 06:20:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+names = %w(Harry Spongebob Patrick)
+
+def badge_maker(name)
+  badge = "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = names.collect {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(names)
+  assignments = names.each_with_index.collect {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"} 
+end
+
+def printer(names)
+  badges = batch_badge_creator(names)
+  room_assignments = assign_rooms(names)
+  badges.each do |index|
+    puts index
+  end
+  room_assignments.each do |index|
+    puts index
+  end
+end
